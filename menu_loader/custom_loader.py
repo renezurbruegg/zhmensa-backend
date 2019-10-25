92753c21015bd0f056e4408cf6c614154f980dd2
renezurbruegg <rene.zurbruegg@gmail.com>
2019-10-25 16:03:59 +0200

    Created abstract class as skeleton for custom loaders for custom mensas

---
+++ menu_loader/custom_loader.py
@@ -0,0 +1,56 @@
+from typing import List
+from abc import ABC, abstractmethod
+
+
+class CustomMenu:
+    def __init__(self):
+        self.mensa: str = ""
+        self.name: str = ""
+        self.id: str = ""
+        self.prices: dict = {}
+        self.isVegi: bool = True
+        self.allergene: List[str] = []
+        self.date: str = ""
+        self.description: List[str] = []
+        self.nutritionFacts: List[str] = []
+        self.lang: str = ""
+        self.link: str = None
+        self.origin: str = "custom loader"
+        self.mealType: str = "lunch"
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "mensaName": self.mensa,
+            "prices": self.prices,
+            "description": self.description,
+            "isVegi": self.isVegi,
+            "allergen": self.allergene,
+            "date": str(self.date),
+            "mealType": self.mealType,
+            "menuName": self.name,
+            "origin": self.origin,
+            "nutritionFacts": self.nutritionFacts,
+            "lang": self.lang,
+            "link": self.link
+        }
+class CustomMensaEntry:
+    def __init__(self):
+        self.name: str = ""
+        self.isOpen: bool = True
+        self.category: str = ""
+        self.openings: dict = None
+
+
+class CustomLoader(ABC):
+    def __init__(self, baseDate, lang):
+        self.baseDate = baseDate
+        self.lang = lang
+
+    @abstractmethod
+    def getAvailableMensas(self) -> List[CustomMensaEntry]:
+        pass
+
+    @abstractmethod
+    def getMenusForMensa(self, mensaInformation) -> List[CustomMenu]:
+        pass
